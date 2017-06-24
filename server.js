56e530beffc7a6dcad58aa90efdd77c4ecc5adee
Rene1992 <hannisenrene1992@gmail.com>
2017-06-25 01:39:27 +0300

    points improvement

--- server.js
+++ server.js
@@ -31,6 +31,37 @@ function randomNumber(lowNum, highNum) {
     return Math.round(randNum);
 }
 
+// function that returns points 
+// by guesscounter value
+function pointMultiplier(guesscounter) {
+    var points;
+    if(guesscounter == 1) {
+        return points = 1000;
+    } else if(guesscounter == 2) {
+        return points = 750;
+    } else if(guesscounter == 3) {
+        return points = 500;
+    } else if(guesscounter == 4) {
+        return points = 400;
+    } else if(guesscounter == 5) {
+        return points = 300;
+    } else if(guesscounter == 6) {
+        return points = 200;
+    } else if(guesscounter >= 7) {
+        return points = 100;
+    }      
+}
+
+// raises guesscounter property by 1 in
+// users array
+function raiseGuessCounter(user) {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id === user) {
+            users[i].guesscounter += 1;
+        }
+    }
+    }
+    
 // this variable calls the randomNumber function 
 // with parameters of 1 and 100
 // so the rightnumber is something between 1 and 100
@@ -47,14 +78,13 @@ io.sockets.on('connection', function (socket) {
     // setting the user and nicknames variables
     var user;
     var nicknames;
-    
+    var socketId = socket.id;
     // socket to get the nickname from the client
     socket.on('nickname_to_server', function(data){
         var nicknames = data['message'];
         
         var alertnick = "Nickname is already taken! Input another nickname";
-        // getting socket id
-        socketId = socket.id;
+        
         for(x=0; x < users.length; x++) {
             if(users[x].name === nicknames) {
                 socket.emit('alertnick', alertnick);
@@ -84,10 +114,15 @@ io.sockets.on('connection', function (socket) {
     //When client sends its message to server event 
     socket.on('message_to_server', function (data) {
         var guessNum = data['message'];
+        
         // check if the number that user submitted
         // matches the right number
         if (rightNum == guessNum) {
-
+            
+            // raises guesscounter variable by id
+            // calling raiseGuessCounter function
+            raiseGuessCounter(socketId);
+            
             // store the socket id that won to socketWon variable
             socketWon = socket.id;
             
@@ -95,10 +130,13 @@ io.sockets.on('connection', function (socket) {
             // userwon variable
             for(var i=0; i < users.length; i++) {
                 if(users[i].id === socketWon){
-                    var userwon = users[i].name;                   
+                    var userwon = users[i].name; 
+                    var guesscounter = users[i].guesscounter;
                 } 
             }
             
+            var points = pointMultiplier(guesscounter);
+                        
             // take the right number that player guessed to 
             // a variable
             guessNum = "The right number was " + "'" + rightNum + "'" + 
@@ -109,7 +147,7 @@ io.sockets.on('connection', function (socket) {
             
             // alert message containing the right number 
             // and the player that won
-            var alertMsg = [userwon,rightNum];
+            var alertMsg = [userwon,rightNum,guesscounter,points];
             // emit the alert to all players
             socket.broadcast.emit('alert', alertMsg); 
             socket.emit('alertwinner', alertMsg);
@@ -118,18 +156,22 @@ io.sockets.on('connection', function (socket) {
             for(var i=0; i < users.length; i++) {
                 if(users[i].id === socketWon){
                     users[i].won += 1;
+                    users[i].points += points;
+                    users[i].guesscounter = 0;
                     } 
                 // update the users
               updateUsers();  
-            }
+            }          
             // set the new number for a new game
             rightNum = randomNumber(1,100);
            // else if players guess is too big 
         } else if (rightNum < guessNum) {
+            raiseGuessCounter(socketId);
             guessNum = "Your guess " + "'" + guessNum + "'" + " is too big!";
             socket.emit("number", guessNum);            
             // else if players guess is too small 
         } else if (rightNum > guessNum) {
+            raiseGuessCounter(socketId);
             guessNum = "Your guess " + "'" + guessNum + "'" + " is too small!";
             socket.emit("number", guessNum);
         }        
@@ -142,7 +184,9 @@ var addUser = function (data, name) {
     var user = {
         id: data,
         name: name,
-        won: 0
+        won: 0,
+        points: 0,
+        guesscounter: 0
     };   
     users.push(user);
     updateUsers();
@@ -164,12 +208,12 @@ var removeUser = function (user) {
 }
 };
 
-// function that updates players
+// function that updates players and shows them
 var updateUsers = function () {
     var str = [];
     for (var i = 0; i < users.length; i++) {
         var user = users[i];
-        str = str + user.name + " " + user.won + "<br>";
+        str = str + user.name + "  /  " + user.won + "  /  " + user.points + "<br>";
 
     }
 
